9d2aadb31e370f5276ebc93d543670a8eb6e49eb
근원 이 <dolees55@gmail.com>
2021-10-06 21:35:19 +0900

    feat : 타일맵 툴

---
+++ 1006/TilemapToolScene.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Config.h"
+#include "GameEntity.h"
+
+
+
+
+
+class Image;
+class Button;
+class ButtonFunction;
+class TilemapToolScene : public GameEntity
+{
+private:
+	SAMPLE_TILE_INFO sampleTileInfo[SAMPLE_TILE_COUNT_Y * SAMPLE_TILE_COUNT_X];
+	TILE_INFO tileInfo[TILE_COUNT_Y * TILE_COUNT_X];
+	Image* sampleImage;
+
+	ButtonFunction* btnFunction;
+
+	Button* saveBtn;
+	Button* loadBtn;
+	ArgumentFuncPtr* arg;
+
+	SAMPLE_TILE_INFO	selectedSampleTile;
+
+public:
+	virtual HRESULT Init() override;
+	virtual void Update() override;
+	virtual void Render(HDC hdc) override;
+	virtual void Release() override;
+
+	void Save(int saveIndex=0);
+	void Load(int loadIndex=0);
+};
+
